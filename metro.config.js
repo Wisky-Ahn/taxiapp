eb7b9990b5991959411c40f807a2481e66e1354a
Wisky_Ahn <ican8087@naver.com>
2024-09-30 17:56:47 +0900

    서버추가

--- metro.config.js
+++ metro.config.js
@@ -6,6 +6,15 @@ const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
  *
  * @type {import('metro-config').MetroConfig}
  */
-const config = {};
+const defaultConfig = getDefaultConfig(__dirname);
 
-module.exports = mergeConfig(getDefaultConfig(__dirname), config);
+const customConfig = {
+    watchman: false, // watchman 비활성화
+    maxWorkers: 2, // 워커 수 제한
+    resolver: {
+        sourceExts: ['js', 'json', 'ts', 'tsx'], // 프로젝트 파일 확장자
+    },
+    watchFolders: ['.'], // 현재 폴더에서 감시
+};
+
+module.exports = mergeConfig(defaultConfig, customConfig);
